be95ce93b12887faa732d4c5a5608d75dcf34150
Sagramielgo <sagramielgo@gmail.com>
2021-04-05 10:26:51 +0200

    upgrade twitterBtn link by including user data

--- src/js/06fetch.js
+++ src/js/06fetch.js
@@ -3,6 +3,8 @@ const cardResultElement = document.querySelector('.js-card-result');
 const createdSection = document.querySelector('.js-created');
 const errorElement = document.querySelector('.js-consoleError');
 const errorSection = document.querySelector('.js-error');
+const shareTwitterBtn = document.querySelector('.js-twitterBtn');
+const shareTwitter = document.querySelector('.js-twitter');
 
 function handleCreateBtn(ev) {
   ev.preventDefault();
@@ -21,9 +23,9 @@ function handleCreateBtn(ev) {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log('Server response:', data);
       if (data.success === true) {
-        cardResultElement.innerHTML = 'Haz click aquí para ver tu tarjeta';
+        cardResultElement.innerHTML =
+          '<h3 class="cardCreated">Haz click aquí para ver tu tarjeta<h3>';
         cardResultElement.href = data.cardURL;
         createdSection.classList.remove('created--hidden');
         errorSection.classList.add('created--hidden');
@@ -36,3 +38,13 @@ function handleCreateBtn(ev) {
 }
 
 createBtn.addEventListener('click', handleCreateBtn);
+
+function handleTwitterBtn() {
+  const tweetText = encodeURIComponent(
+    '¡Echa un vistazo a la tarjeta que me he creado con This Is Me Profile Cards!'
+  );
+  const tweetHashtag = encodeURIComponent('Adalab,promoL,thisIsMe,frontend');
+  const generatedCardURL = cardResultElement.href;
+  shareTwitter.href = `https://twitter.com/intent/tweet?text=${tweetText}&url=${generatedCardURL}&hashtags=${tweetHashtag}`;
+}
+shareTwitterBtn.addEventListener('click', handleTwitterBtn);
